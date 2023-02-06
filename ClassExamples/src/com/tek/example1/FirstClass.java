e757e409c7ea60fa4c136f8a0658f4597593e475
Justin Vititoe <jvit1985@yahoo.com>
2023-02-06 10:46:15 -0600

    added a new class

--- ClassExamples/src/com/tek/example1/FirstClass.java
+++ ClassExamples/src/com/tek/example1/FirstClass.java
@@ -2,8 +2,20 @@ package com.tek.example1;
 
 public class FirstClass {
 	
+	/*
+	 * This can be used for a much longer comment that
+	 * spans multiple lines
+	 */
+	
 	public static void main( String[] args ) {
 		System.out.println("First java program");
+		
+		for (int i = 1; i < 5; i++) {
+			
+		}
+		
+		//this is a comment
+		String s = "some text with a \" and a \\ in it";
 		
 		int i = 5 + 5;
 		System.out.println("5 + 5 = " +i);
