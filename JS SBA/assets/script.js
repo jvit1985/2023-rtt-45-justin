6fc36784db6825cba3c7fba581ca06143492b4c9
Justin Vititoe <jvit1985@yahoo.com>
2023-03-30 11:37:40 -0500

    updated springBoot app again to fix issues

--- JS SBA/assets/script.js
+++ JS SBA/assets/script.js
@@ -518,23 +518,7 @@ $(document).ready(function() {
 
     $('#csv').text(exportJSONToCSV(jsonObject));
 });
-
-// function ConvertToCSV(objArray) {
-//     var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-//     var str = '';
-
-//     for (var i = 0; i < array.length; i++) {
-//         var line = '';
-//         for (var index in array[i]) {
-//             if (line != '') line += ','
-
-//             line += array[i][index];
-//         }
-
-//         str += line + '\r\n';
-//     }
-//     return str;
-// }
+ 
 
 function exportJSONToCSV(objArray) {
     var arr = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
